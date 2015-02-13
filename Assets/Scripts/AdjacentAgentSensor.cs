428bac13d48fb4ae63548d5e8d6eb1ffa6ec0be6
Wil Wilhite <wwilhite89@gmail.com>
2015-02-13 12:49:34 -0500

    Added agents.

---
+++ Assets/Scripts/AdjacentAgentSensor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjacentAgentSensor : MonoBehaviour {
+	public float range;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
