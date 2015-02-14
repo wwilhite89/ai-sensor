ef583748aa06cd2d5ccfe82ee8d67dc13188da32
Adam Stack <bigstack80@knights.ucf.edu>
2015-02-14 15:45:15 -0500

    added three rangefinders and fixed bugs.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,6 +11,8 @@ public class PlayerScript : MonoBehaviour {
 	//player speeds
 	public float speed;
 	public float rotationSpeed;
+	public float playerOffset;
+	public float sensorLength;
 
 	void update() {
 
@@ -27,16 +29,41 @@ public class PlayerScript : MonoBehaviour {
 			transform.Translate(0, translation, 0);
 			transform.Rotate(0, 0, -rotation);
 
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, this.transform.up, 3.0f, 1 << 8);
+		Raycast (); // call my raycasting method
+	}
+
+	public void Raycast() {
+
+		// raycast for walls in the scene, walls are on layer 8
 		Debug.DrawRay (transform.position, this.transform.up * 3.0f, Color.cyan);
+		
+		// right forward wall sensor
+		Vector2 fwdRight = transform.position;
+		fwdRight.Set (this.transform.up.x / 2 + this.transform.right.x / 2, this.transform.up.y / 2 + this.transform.right.y / 2);
+		Debug.DrawRay (transform.position, fwdRight * sensorLength, Color.cyan);
+		
+		// left forward wall sensor
+		Vector2 fwdLeft = transform.position;
+		fwdLeft.Set (this.transform.up.x / 2 + -this.transform.right.x / 2, this.transform.up.y / 2 + -this.transform.right.y / 2);
+		Debug.DrawRay (transform.position, fwdLeft * sensorLength, Color.cyan);
+		
+		RaycastHit2D hitFront = Physics2D.Raycast(transform.position, this.transform.up, sensorLength, 1 << 8);
+		RaycastHit2D hitRight = Physics2D.Raycast(transform.position, fwdRight, sensorLength, 1 << 8);
+		RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, fwdLeft, sensorLength, 1 << 8);
 
-		if (hit.collider != null) {
-						float distance = Mathf.Abs (hit.point.y - transform.position.y);
-	
-						Debug.Log ("The dist is " + distance.ToString("F2"));
-				}
+		hitFront.distance -= playerOffset;
+		hitLeft.distance -= playerOffset;
+		hitRight.distance -= playerOffset;
 
-
+		if (hitFront.collider == null)
+						hitFront.distance = sensorLength;
+		if (hitLeft.collider == null)
+						hitLeft.distance = sensorLength;
+		if (hitRight.collider == null)
+						hitRight.distance = sensorLength;
+		Debug.Log ("FrontSensor " + hitFront.distance.ToString("F2") + " " +
+		           "RightSensor " + hitRight.distance.ToString("F2") +  " " +
+		           "LeftSensor " + hitLeft.distance.ToString("F2"));
 	}
 }
 
