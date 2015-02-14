bc0dab4fe999b8c1e6b66f0bc24b802e2ac296d3
Adam Stack <bigstack80@knights.ucf.edu>
2015-02-14 15:55:19 -0500

    added comments and readability

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-
-	//player speeds
+	//variables 
 	public float speed;
 	public float rotationSpeed;
 	public float playerOffset;
 	public float sensorLength;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		}
 
 	void update() {
 
@@ -32,35 +33,43 @@ public class PlayerScript : MonoBehaviour {
 		Raycast (); // call my raycasting method
 	}
 
+	// create three raycasting sensors
 	public void Raycast() {
 
-		// raycast for walls in the scene, walls are on layer 8
-		Debug.DrawRay (transform.position, this.transform.up * 3.0f, Color.cyan);
-		
-		// right forward wall sensor
+		// vectors to hold sensor right offset and left offset
 		Vector2 fwdRight = transform.position;
-		fwdRight.Set (this.transform.up.x / 2 + this.transform.right.x / 2, this.transform.up.y / 2 + this.transform.right.y / 2);
-		Debug.DrawRay (transform.position, fwdRight * sensorLength, Color.cyan);
-		
-		// left forward wall sensor
 		Vector2 fwdLeft = transform.position;
+
+		// set the sensor offsets
+		fwdRight.Set (this.transform.up.x / 2 + this.transform.right.x / 2, this.transform.up.y / 2 + this.transform.right.y / 2);
 		fwdLeft.Set (this.transform.up.x / 2 + -this.transform.right.x / 2, this.transform.up.y / 2 + -this.transform.right.y / 2);
+
+
+		// Debug rays drawn in scene view  ***********  May want to remove  ************
+		Debug.DrawRay (transform.position, this.transform.up * 3.0f, Color.cyan);
+		Debug.DrawRay (transform.position, fwdRight * sensorLength, Color.cyan);
 		Debug.DrawRay (transform.position, fwdLeft * sensorLength, Color.cyan);
-		
+
+		// three raycasts fwd, left and right.
+		// theses rays will only sense objects in layer 8, that is where the walls live
 		RaycastHit2D hitFront = Physics2D.Raycast(transform.position, this.transform.up, sensorLength, 1 << 8);
 		RaycastHit2D hitRight = Physics2D.Raycast(transform.position, fwdRight, sensorLength, 1 << 8);
 		RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, fwdLeft, sensorLength, 1 << 8);
 
+		// for accuracy and readability, player offset is the radious of its collider
 		hitFront.distance -= playerOffset;
 		hitLeft.distance -= playerOffset;
 		hitRight.distance -= playerOffset;
 
+		// return the sensor length if the sensor does not sense a wall/ other collider
 		if (hitFront.collider == null)
 						hitFront.distance = sensorLength;
 		if (hitLeft.collider == null)
 						hitLeft.distance = sensorLength;
 		if (hitRight.collider == null)
 						hitRight.distance = sensorLength;
+
+		// print the distances found to the console
 		Debug.Log ("FrontSensor " + hitFront.distance.ToString("F2") + " " +
 		           "RightSensor " + hitRight.distance.ToString("F2") +  " " +
 		           "LeftSensor " + hitLeft.distance.ToString("F2"));
