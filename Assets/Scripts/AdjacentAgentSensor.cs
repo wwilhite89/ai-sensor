93629488f8da2bb1ad983bbe2d732f998d9fb2e4
Wil Wilhite <wwilhite89@gmail.com>
2015-02-15 16:18:29 -0500

    Added Canvas to display outputs.

--- Assets/Scripts/AdjacentAgentSensor.cs
+++ Assets/Scripts/AdjacentAgentSensor.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class AdjacentAgentSensor : MonoBehaviour {
-	public ArrayList adjacentAgents;
+	public ArrayList adjacentAgents = new ArrayList();
 
 	public void Sense(Collider2D col, GameObject gameObject) {
+//		adjacentAgents = new ArrayList();
 		if(col.gameObject.tag == "Agent") {
 			float distance = Vector3.Distance(gameObject.transform.position, col.gameObject.transform.position);
 			float rotation = Mathf.Abs(gameObject.transform.rotation.eulerAngles.z - col.gameObject.transform.rotation.eulerAngles.z);
-			Debug.Log(distance);
-			Debug.Log(rotation);
+			Vector2 vector = new Vector2(distance, rotation);
+			adjacentAgents.Add(vector);
+//			Debug.Log(distance);
+//			Debug.Log(rotation);
 		}
 	}
 }
