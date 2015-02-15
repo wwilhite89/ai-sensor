984ecd99248f19b334cd39973496667070be1c16
Jason Braswell <jason.braswell@knights.ucf.edu>
2015-02-15 17:53:55 -0500

    Added initial script for pie sensor

---
+++ Assets/Scripts/PieSensorScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieSensorScript : MonoBehaviour {
+
+    public float range = 3F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate() {
+        GameObject[] agents = GameObject.FindGameObjectsWithTag("Agent");
+        
+        for (int i = 0; i < agents.Length; i++) {
+            Vector3 distance = agents[i].transform.position - gameObject.transform.position;
+
+            if (distance.magnitude <= range)
+            {
+                Debug.Log(agents[i].name + " is within " + range + " units.");
+                //Vector3.Angle(gameObject.transform.ro
+            }
+        }
+    }
+}
