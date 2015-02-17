96f1678697108e33f356430e1b413269cd4e54d0
Adam Stack <bigstack80@knights.ucf.edu>
2015-02-16 19:55:53 -0500

    added a spawn agent script and scene just in case, for testing

---
+++ Assets/Scripts/CreateAgent.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateAgent : MonoBehaviour {
+
+	public float minSpawnTime = 0.75f;
+	public float maxSpawnTime = 2f;
+	public GameObject Agent;
+
+	private Vector3 agentLocation;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("SpawnAgent", minSpawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SpawnAgent()
+	{
+		// cameras current position and bounds
+		Camera camera = Camera.main;
+		Vector3 cameraPos = camera.transform.position;
+		
+		float xMax = camera.aspect * camera.orthographicSize;
+		float xRange = camera.aspect * camera.orthographicSize * 1.75f;
+		float yMax = camera.orthographicSize - 0.5f;
+		
+		// create a new cat position at random location at same z position
+		Vector3 catPos = new Vector3 (cameraPos.x + 
+		                              Random.Range (xMax - xRange, xMax),
+		                              Random.Range (-yMax, yMax),
+		                              Agent.transform.position.z);
+		
+		// create an instance of the cat prefab at location catPos
+		Instantiate (Agent, catPos, Quaternion.identity);
+		Invoke ("SpawnAgent", Random.Range (minSpawnTime, maxSpawnTime));
+	}
+}
